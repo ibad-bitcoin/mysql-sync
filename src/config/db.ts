c27f31672e0132861bed2eafb27a553974618d88
Abdul Ibad <ibad@bitcoin.co.id>
2018-10-19 16:19:08 +0700

    move db config to app

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,21 +1,12 @@
+import app from './app'
 import * as mysql from 'mysql'
 
 class DatabaseConnection {
 
-    public source = mysql.createPool({
-        host     : 'localhost',
-        user     : 'chat_usr',
-        password : 'secret',
-        database : 'chatindodax'
-    });
+    public source = mysql.createPool(app.DB_SOURCE);
 
-    public destination = mysql.createPool({
-        host     : 'localhost',
-        user     : 'chat_usr',
-        password : 'secret',
-        database : 'chatsync'
-    })
-    
+    public destination = mysql.createPool(app.DB_DESTINATION)
+
 }
 
 export default new DatabaseConnection()
