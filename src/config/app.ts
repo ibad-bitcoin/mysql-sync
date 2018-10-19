a58d6ee86e87c8a527f5594767eb2411cec2ae7d
Abdul Ibad <ibad@bitcoin.co.id>
2018-10-19 16:18:44 +0700

    change config to dotenv

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,23 @@
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 class Config {
     public RUN_INITIAL = false
-    public SYNC_INTERVAL = 30*1000
-    public ROW_LIMIT = 20000
+    public SYNC_INTERVAL = parseInt(process.env.SYNC_INTERVAL) || 30*1000
+    public ROW_LIMIT = parseInt(process.env.ROW_LIMIT) || 20000
+    public DB_SOURCE = {
+        host     : process.env.SOURCE_HOST,
+        user     : process.env.SOURCE_USER,
+        password : process.env.SOURCE_PASS,
+        database : process.env.SOURCE_DB
+    }
+    public DB_DESTINATION = {
+        host     : process.env.DEST_HOST,
+        user     : process.env.DEST_USER,
+        password : process.env.DEST_PASS,
+        database : process.env.DEST_DB
+    }
 }
 
 export default new Config()
